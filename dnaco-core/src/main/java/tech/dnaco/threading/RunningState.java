7d2d8cbc85bdff2e73ca93df406f89d4dcd70b3b
Matteo Bertozzi <theo.bertozzi@gmail.com>
2021-04-10 19:22:24 +0200

    add RunninState interface and ServiceRunningState impl

---
+++ dnaco-core/src/main/java/tech/dnaco/threading/RunningState.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.dnaco.threading;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public interface RunningState {
+  boolean isRunning();
+
+  public class ServiceRunningState implements RunningState {
+    private final AtomicBoolean running;
+
+    public ServiceRunningState() {
+      this(true);
+    }
+
+    public ServiceRunningState(final boolean state) {
+      this(new AtomicBoolean(state));
+    }
+
+    public ServiceRunningState(final AtomicBoolean running) {
+      this.running = running;
+    }
+
+    public void start() {
+      running.set(true);
+    }
+
+    public void stop() {
+      running.set(false);
+    }
+
+    @Override
+    public boolean isRunning() {
+      return running.get();
+    }
+  }
+}
